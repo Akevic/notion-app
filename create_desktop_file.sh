d45ff8ef0248fffc5a4da9c6ad01dc79de6dca55
David Johns <sysdrum@gmail.com>
2019-09-08 07:09:23 -0500

    push

---
+++ create_desktop_file.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+WORKING_DIR=`pwd`
+THIS_PATH=`readlink -f $0`
+cd `dirname ${THIS_PATH}`
+FULL_PATH=`pwd`
+cd ${WORKING_DIR}
+cat <<EOS > Notion.desktop
+[Desktop Entry]
+Name=Notion
+Name[en_US]=Notion.so
+Comment=Notion.so Desktop application for Linux
+Exec="${FULL_PATH}/notion-app"
+Terminal=false
+Categories=Application;
+Type=Application
+Icon=${FULL_PATH}/notion.png
+EOS
+chmod +x Notion.desktop
+## This can be updated if this path is not valid. 
+## cp -p Notion.desktop ~/.local/share/applications
+[ ! -d "$~/.local/share/applications" ] && { cd ~/.local/share; mkdir applications; cd ${WORKING_DIR}; cp -p Notion.desktop ~/.local/share/applications; } || { cd ${WORKING_DIR}; cp -p Notion.desktop ~/.local/share/applications; }
